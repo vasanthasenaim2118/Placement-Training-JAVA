38d69e1ff8c464d4c5405cc7a90842ec864d81f7
VASANTHASENAI M <vasanthasenaim@gmail.com>
2025-08-13 10:21:22 +0530

    BoxPatterns.java

---
+++ BoxPatterns.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        int r,c,n;
+        n=sc.nextInt();
+        for(r=1;r<=n;r++){
+            for(c=1;c<=n;c++){
+                if(r==1||r==n||c==1||c==n){
+                    System.out.print("1");
+                }else{
+                    System.out.print("0");
+                }
+                
+            }
+            System.out.println();
+        }
+    }
+}
