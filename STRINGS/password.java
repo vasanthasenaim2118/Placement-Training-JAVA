426326dddfdbb80112feed9e3744aa91737a3bac
VASANTHASENAI M <vasanthasenaim@gmail.com>
2025-08-28 21:33:15 +0530

    password.java
    C-LEVEL0-MOD9-Strings-Challenge-7
    
    Write a program to accept the string from the user and display the string in the password format ("*") without making any change in the input array

---
+++ STRINGS/password.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class password {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        for(int i=0;i<s.length();i++){
+                System.out.print("*");
+        }
+    }
+}
