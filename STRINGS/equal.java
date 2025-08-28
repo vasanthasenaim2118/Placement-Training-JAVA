2dd3cb7ec6aad1e4ac472a6e2161d1f068f94cd1
VASANTHASENAI M <vasanthasenaim@gmail.com>
2025-08-28 21:51:49 +0530

    equal.java
    C-L1-MOD 9-STRING 2
    
    Write a program to check given string are equal or not equal

---
+++ STRINGS/equal.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        
+        if(s1.equals(s2)){
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not Equal");
+        }
+        }
+}
