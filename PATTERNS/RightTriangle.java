fba3d4bccaf926176aee0d383b8d6e59d8c27012
VASANTHASENAI M <vasanthasenaim@gmail.com>
2025-08-28 21:31:59 +0530

    RightTriangle.java

---
+++ PATTERNS/RightTriangle.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class rightTriangle {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int row,col,n;
+        n = sc.nextInt();
+        for(row=1;row<=n;row++){
+            for(col=1;col<=row;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
